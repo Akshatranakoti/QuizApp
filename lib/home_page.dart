ce3078c6430f8a3e4b0e4a41965f8f9c5cce1b6d
Akshat Ranakoti <akshatranakoti01@gmail.com>
2023-08-23 12:38:23 +0530

    HomePage added

---
+++ lib/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient:
+                LinearGradient(colors: [Colors.deepOrange, Colors.deepPurple])),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                "assets/images/quiz-logo.png",
+                width: 200,
+                color: Color.fromARGB(152, 249, 249, 249),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text("Let learn flutter",
+                  style: TextStyle(color: Colors.white)),
+              const SizedBox(
+                height: 20,
+              ),
+              OutlinedButton.icon(
+                  onPressed: () {},
+                  style:
+                      OutlinedButton.styleFrom(foregroundColor: Colors.white),
+                  icon: Icon(Icons.arrow_right_alt),
+                  label: const Text("quiz"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
